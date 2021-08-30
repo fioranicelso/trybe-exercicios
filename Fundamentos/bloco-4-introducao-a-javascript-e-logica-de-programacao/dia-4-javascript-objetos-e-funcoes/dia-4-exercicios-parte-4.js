cfca542004a196177c4c0408348f6904c85b605d
Celso Fiorani <celsofiorani@gmail.com>
2021-08-30 18:52:59 -0300

    parte 4

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/dia-4-exercicios-parte-4.js
@@ -0,0 +1,14 @@
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info.recorrente = "Sim";
+
+for (todosOsValores in info){
+    console.log(info[todosOsValores]);
+}
+
+
