1b6bc90ec31e0eeff567ae4649fd55ebaceb535b
Celso Fiorani <celsofiorani@gmail.com>
2021-09-02 15:33:08 -0300

    dia 4.2

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte-2-Funções/dia-4-exercicios-parte-2-Function.js
@@ -0,0 +1,15 @@
+
+let numbers= [2,3,6,7,10,1];
+
+
+function returnNumberSelected (numbers){
+ for (let index = 0; index < numbers.length; index+=1) {
+     for (let index2 = 1; index2 < numbers.length; index2+=1){
+       if  (index>index2){
+           let maiorNumero 
+       }         
+     }
+     
+ }   
+}
+console.log (maiorNumero)
